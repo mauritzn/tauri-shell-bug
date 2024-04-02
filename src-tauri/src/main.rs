1667c5b8556c4b1bcd6c315e4e7b1024628016ea
Mauritz N <lnouwq9e@duck.com>
2024-04-02 22:40:36 +0300

    Update to auto open web devtools

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,18 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
+
 fn main() {
     tauri::Builder::default()
+        .setup(|app| {
+            #[cfg(debug_assertions)] // only include this code on debug builds
+            {
+                let window = app.get_window("main").unwrap();
+                window.open_devtools();
+            }
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");

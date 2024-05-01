3623ab0348b0dd90e503a675430d6df810a8361e
Mauritz N <lnouwq9e@duck.com>
2024-05-01 06:22:39 +0300

    Add workaround using "invoke" and Rust
    I'm sure this workaround isn't perfect, but it does seem to work even for really large command outputs. This commit obviously doesn't fix the 2 previous methods of running commands using "Command", instead it adds a third method of running commands, using "invoke" and Rust.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,41 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use regex::Regex;
+use std::process::Command;
 use tauri::Manager;
+
+#[tauri::command]
+async fn print_py_numbers(python_script_path: String) -> Result<String, String> {
+    let regex = Regex::new(r"\S+__print_numbers[.]py$").expect("Invalid regex pattern");
+
+    /*
+     * This regex is used to offer similar argument safety as the `tauri.conf.json` validator: { "validator": "\\S+__print_numbers[.]py$" },
+     * this could obviously be avoided if the script path was handled in Rust as well,
+     * but I wanted to keep this as similar as possible to the JS `Command` version.
+     */
+    if regex.is_match(&python_script_path) == false {
+        return Err(format!(
+            "Provided Python script path does not match expected filename! Got: {}",
+            python_script_path
+        ));
+    }
+
+    let output = Command::new("python")
+        .arg(python_script_path)
+        .output() // Executes the command and captures the output
+        .map_err(|err| err.to_string())?;
+
+    if output.status.success() {
+        // If the command was successful, print the output
+        let output_str = String::from_utf8_lossy(&output.stdout);
+        Ok(output_str.to_string())
+    } else {
+        // If the command failed, handle the error
+        let error_message = String::from_utf8_lossy(&output.stderr);
+        Err(format!("Error: {}", error_message))
+    }
+}
 
 fn main() {
     tauri::Builder::default()
@@ -13,7 +47,7 @@ fn main() {
             }
             Ok(())
         })
-        .invoke_handler(tauri::generate_handler![])
+        .invoke_handler(tauri::generate_handler![print_py_numbers])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { Command } from "@tauri-apps/api/shell";
 import { exists } from "@tauri-apps/api/fs";
 import { join, resolve, dirname } from "@tauri-apps/api/path";
+import { invoke } from "@tauri-apps/api";
 
 async function findPythonScript(): Promise<string> {
   let currentFolder = await resolve(".");
@@ -18,8 +19,20 @@ async function findPythonScript(): Promise<string> {
   }
 }
 
+async function invokeProcess(pythonScriptPath: string) {
+  console.log("Running python script (with invoke workaround)...");
+
+  try {
+    const output = await invoke("print_py_numbers", { pythonScriptPath });
+    console.log(output);
+    console.log("Done...");
+  } catch (err) {
+    console.error(`command error: "${err}"`);
+  }
+}
+
 async function executeProcess(pythonScriptPath: string) {
-  console.log("Running python script (with execute)...");
+  console.log("Running python script (with JS Command execute)...");
 
   let process = await new Command("python-test", [pythonScriptPath]).execute();
 
@@ -49,7 +62,7 @@ async function eventProcess(pythonScriptPath: string) {
     console.log(`command stderr: "${line}"`);
   });
 
-  console.log("Running python script (with events)...");
+  console.log("Running python script (with JS Command events)...");
   const child = await process.spawn();
   console.log("pid:", child.pid);
 }
@@ -57,6 +70,9 @@ async function eventProcess(pythonScriptPath: string) {
 window.addEventListener("DOMContentLoaded", () => {
   findPythonScript()
     .then((pythonScriptPath) => {
+      const invokeWorkaroundFormEl = document.querySelector(
+        "form#invokeWorkaroundForm"
+      ) as HTMLFormElement | null;
       const executeFormEl = document.querySelector(
         "form#executeForm"
       ) as HTMLFormElement | null;
@@ -64,20 +80,43 @@ window.addEventListener("DOMContentLoaded", () => {
         "form#eventForm"
       ) as HTMLFormElement | null;
 
-      if (executeFormEl) {
-        const executeFormButtonEl = executeFormEl.querySelector(
+      if (invokeWorkaroundFormEl) {
+        const submitBtnEl = invokeWorkaroundFormEl.querySelector(
           "button"
         ) as HTMLButtonElement | null;
-        if (executeFormButtonEl) {
+        if (submitBtnEl) {
+          invokeWorkaroundFormEl.addEventListener("submit", (event) => {
+            event.preventDefault();
+
+            submitBtnEl.innerText = "Running...";
+            submitBtnEl.disabled = true;
+
+            invokeProcess(pythonScriptPath).finally(() => {
+              submitBtnEl.innerText = "Start";
+              submitBtnEl.disabled = false;
+            });
+          });
+        } else {
+          console.warn(`missing "form#executeForm button" element!`);
+        }
+      } else {
+        console.warn(`missing "form#invokeWorkaroundForm" element!`);
+      }
+
+      if (executeFormEl) {
+        const submitBtnEl = executeFormEl.querySelector(
+          "button"
+        ) as HTMLButtonElement | null;
+        if (submitBtnEl) {
           executeFormEl.addEventListener("submit", (event) => {
             event.preventDefault();
 
-            executeFormButtonEl.innerText = "Running...";
-            executeFormButtonEl.disabled = true;
+            submitBtnEl.innerText = "Running...";
+            submitBtnEl.disabled = true;
 
             executeProcess(pythonScriptPath).finally(() => {
-              executeFormButtonEl.innerText = "Start";
-              executeFormButtonEl.disabled = false;
+              submitBtnEl.innerText = "Start";
+              submitBtnEl.disabled = false;
             });
           });
         } else {
@@ -88,19 +127,19 @@ window.addEventListener("DOMContentLoaded", () => {
       }
 
       if (eventFormEl) {
-        const eventFormButtonEl = eventFormEl.querySelector(
+        const submitBtnEl = eventFormEl.querySelector(
           "button"
         ) as HTMLButtonElement | null;
-        if (eventFormButtonEl) {
+        if (submitBtnEl) {
           eventFormEl.addEventListener("submit", (event) => {
             event.preventDefault();
 
-            eventFormButtonEl.innerText = "Running...";
-            eventFormButtonEl.disabled = true;
+            submitBtnEl.innerText = "Running...";
+            submitBtnEl.disabled = true;
 
             eventProcess(pythonScriptPath).finally(() => {
-              eventFormButtonEl.innerText = "Start";
-              eventFormButtonEl.disabled = false;
+              submitBtnEl.innerText = "Start";
+              submitBtnEl.disabled = false;
             });
           });
         } else {
